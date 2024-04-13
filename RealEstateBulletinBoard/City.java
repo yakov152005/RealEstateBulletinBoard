6251e2b16c5950dd2e0c66769bb31087ccd81733
yakov152005 <155822770+yakov152005@users.noreply.github.com>
2024-04-13 16:43:06 +0300

    Delete RealEstateBulletinBoard directory

--- RealEstateBulletinBoard/City.java
+++
@@ -1,9 +0,0 @@
-public class City {
-	private String[] nameCity = {};
-	public final String[] district = {"Negev", "South", "Center","Sharon", "North"};
-	private String[] streets;
-
-	public String toString(){
-		return "CHECK";
-	}
-}
