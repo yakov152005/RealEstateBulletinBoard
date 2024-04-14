26b1a7628b891579194c5e76ab0a5474c7474eee
yakov152005 <155822770+yakov152005@users.noreply.github.com>
2024-04-14 17:28:15 +0300

    Update Street.java

--- RealEstateProgram/src/Street.java
+++ RealEstateProgram/src/Street.java
@@ -1,5 +1,7 @@
 public class Street {
+	
 	private  String nameStreet;
+	
 	public static final String[] streetsList ={
 			"Herzl","Rothschild","David-remez","Ben-Gurion","Hananal","Menachem-Begin","Neve-Dekalim",
 			"Yehuda-Levi","Ben-Zvi","Bialik","Yafa-nof","The-Reef"
