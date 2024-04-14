139ba69aad2bb549d52dfd3ac48587139a1701a5
yakov152005 <155822770+yakov152005@users.noreply.github.com>
2024-04-14 17:29:17 +0300

    Update User.java

--- RealEstateProgram/src/User.java
+++ RealEstateProgram/src/User.java
@@ -1,23 +1,30 @@
 import java.util.Scanner;
 public class User {
+
 	private String username;
 	private String password;
 	private String phoneNumber;
 	private boolean isBroker;
 	private int propertyCount; // מספר הנכסים שפורסמו על ידי המשתמש
+
+	public static Scanner s = new Scanner(System.in);
+
 	public User (String username, String password, String phoneNumber, boolean isBroker){
 		this.username = username;
 		this.password = password;
 		this.phoneNumber = phoneNumber;
 		this.isBroker = isBroker;
 	}
+	
+	//O(1)
 	public boolean isBroker() {
 		return isBroker;
 	}
-	public static Scanner s = new Scanner(System.in);
+	//O(1)
 	public boolean isUserNameEquals(String username){
 		return this.username.equals(username);
 	}
+	//O(1)
 	public boolean checkCredentials (String username, String password) {
 		boolean success = false;
 		if (this.username.equals(username) && this.password.equals(password)) {
