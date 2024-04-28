ba792f1d41c3e5016494b5df7c28c806374fe1b3
yakov152005 <155822770+yakov152005@users.noreply.github.com>
2024-04-28 15:11:48 +0300

    Update Property.java

--- RealEstateProgram/src/Property.java
+++ RealEstateProgram/src/Property.java
@@ -1,9 +1,8 @@
 public class Property {
-	
 	private City city;
 	private Street street;
 	private int numberOfRooms;
-	private int price;
+	private long price;
 	private String typeLand;
 	private boolean forRent;
 	private int houseNumber;
@@ -11,8 +10,8 @@ public class Property {
 	private User owner;
 
 	public static final String[] TYPE_FOR_LAND ={"Regular apartment", "Penthouse", "Private House" };
-	
-	public Property(User owner,City city, Street street,int floorNumber,int numberOfRooms, int houseNumber, boolean forRent, int price, String typeLand){
+
+	public Property(User owner,City city, Street street,int floorNumber,int numberOfRooms, int houseNumber, boolean forRent, long price, String typeLand){
 		this.owner = owner;
 		this.city = city;
 		this.street = street;
@@ -27,24 +26,22 @@ public class Property {
 	public String getTypeLand() {
 		return typeLand;
 	}
-
 	public int getNumberOfRooms() {
 		return numberOfRooms;
 	}
 	public boolean isForRent() {
 		return forRent;
 	}
-
-	public int getPrice() {
+	public long getPrice() {
 		return price;
 	}
 	public User getOwner() {
 		return owner;
 	}
+
 	public String toString(){
 		return getClass().getSimpleName() + "---> " + this.city + "|" +this.street + " " + this.houseNumber + "\n "
 				+ this.typeLand + "|"  + "for rent? " + this.forRent + ": " + this.numberOfRooms + " rooms, floor " + this.floorNumber
 				+ ".\n Price: " + this.price + "$."  + owner.toString();
 	}
-
 }
